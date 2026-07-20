67a73ac477ce6a5be98ed6e6009e5c1dc9cd9f1c
motocor28-boop <motocor28@gmail.com>
2026-07-20 15:30:18 -0400

    Activar cobertura máxima de PID en el escaneo profundo

--- desktop/autoguard_scan_dios_v6_2/app.py
+++ desktop/autoguard_scan_dios_v6_2/app.py
@@ -1,4 +1,8 @@
-from option_b_app import main
+import option_b_app
+from deep_scan_full import DeepScannerFull
+
+option_b_app.DeepScanner = DeepScannerFull
+main = option_b_app.main
 
 
 if __name__ == "__main__":
