109ced80dd063395cf2a7f3462dee6a78eff24da
motocor28-boop <motocor28@gmail.com>
2026-08-11 14:07:45 -0400

    feat: agregar REPUESTOS IA Cloud v2

---
+++ repuestos/config.js
@@ -0,0 +1,12 @@
+window.REPUESTOS_CONFIG={
+  version:'2.0.0',
+  supabaseUrl:'https://gecrjlkfrcrlsjhanfjz.supabase.co',
+  supabaseAnonKey:'sb_publishable_bJlPZe-8xMdmvnVNkInSLg_phBKSoaa',
+  table:'mantpro_records',
+  recordPrefix:'inventory_',
+  allowedEmail:'motocor28@gmail.com',
+  username:'esteban',
+  supervisor:'Esteban Cortez Richards',
+  requireAuth:true,
+  appTitle:'REPUESTOS IA CLOUD'
+};
