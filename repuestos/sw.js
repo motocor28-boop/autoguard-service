1a72af0ab00a0acea000b2c551a8b4191f4befef
motocor28-boop <motocor28@gmail.com>
2026-08-11 16:02:30 -0400

    fix: cache PWA instalable v3.1.1

--- repuestos/sw.js
+++ repuestos/sw.js
@@ -1,5 +1,5 @@
-const CACHE='levantamiento-repuestos-ia-v3-1-0';
-const CORE=['./','index.html','styles.css','config.js','auth.js','app.js','manifest.webmanifest'];
+const CACHE='levantamiento-repuestos-ia-v3-1-1';
+const CORE=['./','index.html','styles.css','config.js','auth.js','app.js','install.js','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(a=>Promise.all(a.filter(x=>x!==CACHE).map(x=>caches.delete(x))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{if(r&&r.ok){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{})}return r}).catch(()=>caches.match(e.request)))});
